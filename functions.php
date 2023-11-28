c7fe2e044753e6e1d32e26bb107a0078e94302b4
root <root@umut.in>
2023-11-28 13:09:18 +0100

    new files

---
+++ functions.php
@@ -0,0 +1,195 @@
+<?php
+global $mysql_host, $mysql_user, $mysql_pass, $mysql_db;
+require ('config.inc.php');
+use Jelix\IniFile\IniException;
+use Jelix\IniFile\IniModifier;
+use Jelix\IniFile\IniModifierInterface;
+
+$db = new mysqli($mysql_host, $mysql_user, $mysql_pass, $mysql_db);
+$ini = new IniModifier('/home/pzserver/Zomboid/Server/pzserver.ini');
+
+function get_mods_from() {
+    global $db;
+    $mods = array();
+    $result = $db->query("SELECT * FROM workshop_id");
+    while($obj = $result->fetch_object()){
+        $mods[$obj->id]['list'] = array();
+        $mods[$obj->id]['name'] = $obj->title;
+        $mods[$obj->id]['active'] = $obj->active;
+        $result2 = $db->query("SELECT * FROM mod_id WHERE workshop_id=".$obj->id);
+        $mods[$obj->id]['list'] = array();
+        while($obj2 = $result2->fetch_object()){
+            $mods[$obj->id]['list'][$obj2->id]['name'] = $obj2->mod_id;
+            $mods[$obj->id]['list'][$obj2->id]['active'] = $obj2->active;
+        }
+    }
+    return $mods;
+}
+function get_workshop_item_for_id($id) {
+    return get_workshop_item_for_array(array($id));
+}
+function get_workshop_item_for_array($arr) {
+
+    $ch = curl_init();
+    $post = "itemcount=".count($arr)."&";
+    for ($i = 0; $i < count($arr); $i++) {
+        $post .= '&publishedfileids['.$i.']='.$arr[$i];
+    }
+
+    curl_setopt($ch, CURLOPT_URL,"https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec($ch);
+
+    curl_close($ch);
+
+    $resp = json_decode($server_output);
+    $matches = array();
+    $matches2 = array();
+    $desc = $resp->response->publishedfiledetails[0]->description;
+    $desc = preg_replace('/\[\/(.*?)\]|\[(.*?)]/', "", $desc);
+    preg_match_all('/Workshop ID: \d+\n/', $desc, $matches);
+    preg_match_all('/Mod ID: (.*?)(\n|$)/', $desc, $matches2);
+    $real_matches = array();
+    foreach ($matches2[0] as $m) {
+        $l = trim(str_replace("Mod ID: ","",$m));
+        if (!in_array($l, $real_matches)) $real_matches[] = $l;
+    }
+    $ret = array();
+    $ret['id'] = $resp->response->publishedfiledetails[0]->publishedfileid;
+    $ret['title'] = $resp->response->publishedfiledetails[0]->title;
+    $ret['wsid'] = trim(str_replace("Workshop ID: ","",$matches[0][0]));
+    $ret['modids'] = $real_matches;
+    return (object) $ret;
+}
+function insert_wsitem($id) {
+    global $db;
+    $wsitem = get_workshop_item_for_id($id);
+    $db->query("DELETE FROM workshop_id WHERE id='".$wsitem->id."'");
+    $db->query("INSERT INTO workshop_id (id, title, active) VALUES ('".$wsitem->id."','".addslashes($wsitem->title)."',0)");
+    $db->query("DELETE FROM mod_id WHERE workshop_id='".$wsitem->id."'");
+    foreach ($wsitem->modids as $mod) {
+        $db->query("INSERT INTO mod_id (workshop_id, mod_id, active) VALUES ('".$wsitem->id."','".addslashes($mod)."',0)");
+    }
+}
+
+/**
+ * @throws IniException
+ */
+function delete_wsitem($id) {
+    global $db;
+    $db->query("DELETE FROM workshop_id WHERE id='".$id."'");
+    $db->query("DELETE FROM mod_id (workshop_id, mod_id, active) WHERE workshop_id='".$id."'");
+    sync_to_file();
+}
+
+/**
+ * @throws IniException
+ */
+function deactivate_module($id) {
+    global $db;
+    $db->query("UPDATE mod_id set active=false WHERE id = ".addslashes($id));
+    sync_to_file();
+}
+
+/**
+ * @throws IniException
+ */
+function deactivate_item($id) {
+    global $db;
+    $db->query("UPDATE workshop_id set active=false WHERE id='".addslashes($id)."'");
+    $db->query("UPDATE mod_id set active=false WHERE workshop_id='".addslashes($id)."'");
+    sync_to_file();
+}
+
+/**
+ * @throws IniException
+ */
+function activate_module($id) {
+    global $db;
+    $db->query("UPDATE mod_id set active=true WHERE id='".addslashes($id)."'");
+    $wsid = $db->query("SELECT * FROM mod_id WHERE id=".$id)->fetch_object()->workshop_id;
+    $db->query("UPDATE workshop_id set active=true WHERE id = ".$wsid);
+    sync_to_file();
+}
+
+/**
+ * @throws IniException
+ */
+function activate_item($id) {
+    global $db;
+    $db->query("UPDATE workshop_id set active=true WHERE id='".$id."'");
+    $db->query("UPDATE mod_id set active=true WHERE workshop_id='".$id."'");
+    sync_to_file();
+}
+function sync_from_steam() {
+    global $db;
+    $result = $db->query("SELECT * FROM workshop_id");
+    while($obj = $result->fetch_object()){
+        $wsitem = get_workshop_item_for_id($obj->id);
+        $db->query("UPDATE workshop_id set title='".addslashes($wsitem->title)."' WHERE id='".$wsitem->id."'");
+        foreach ($wsitem->modids as $mod) {
+            $check = $db->query("SELECT * FROM mod_id WHERE workshop_id='' AND mod_id=''");
+            if ($check->num_rows > 0) {
+                $db->query("UPDATE mod_id SET workshop_id='".$wsitem->id."', mod_id='".addslashes($mod)."' WHERE id=".$check->id);
+            } else {
+                $db->query("INSERT INTO mod_id (workshop_id, mod_id, active) VALUES ('".$wsitem->id."','".addslashes($mod)."',1)");
+            }
+
+        }
+    }
+}
+
+/**
+ * @throws IniException
+ */
+function sync_to_file() {
+    global $db, $ini;
+    $mods = array();
+    $result = $db->query("SELECT * FROM workshop_id WHERE active=true");
+    while($obj = $result->fetch_object()){
+        $mods[$obj->id] = array();
+        $result2 = $db->query("SELECT * FROM mod_id WHERE active=true AND workshop_id=".$obj->id);
+        while($obj2 = $result2->fetch_object()){
+            $mods[$obj->id][] = $obj2->mod_id;
+        }
+    }
+    $idslist = "";
+    $modslist = "";
+    foreach ($mods as $id=>$mod) {
+        $idslist .= $id.";";
+        foreach ($mod as $modname) {
+            $modslist .= $modname.";";
+        }
+    }
+    $idslist = rtrim($idslist, ";");
+    $modslist = rtrim($modslist, ";");
+    $ini->setValue('Mods', $modslist);
+    $ini->setValue('WorkshopItems', $idslist);
+    $ini->save(null, IniModifierInterface::FORMAT_NO_QUOTES);
+}
+
+function runServerCommand($cmd)
+{
+    $restartcommand = 'sudo -H -u pzserver /home/pzserver/webserver restart';
+    $startcommand = 'sudo -H -u pzserver /home/pzserver/webserver start';
+    $stopcommand = 'sudo -H -u pzserver /home/pzserver/webserver stop';
+    $updatecommand = 'sudo -H -u pzserver /home/pzserver/webserver update';
+    switch($cmd) {
+        case "start":
+            shell_exec($startcommand);
+            break;
+        case "stop":
+            shell_exec($stopcommand);
+            break;
+        case "restart":
+            shell_exec($restartcommand);
+            break;
+        case "update":
+            shell_exec($updatecommand);
+            break;
+    }
+}
---
+++ index.php
@@ -0,0 +1,200 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+require ("functions.php");
+
+if (!empty($_POST['wsid'])) {
+    insert_wsitem(addslashes($_POST['wsid']));
+}
+
+if (!empty($_GET['delete'])) {
+    try {
+        delete_wsitem(addslashes($_GET['delete']));
+    } catch (\Jelix\IniFile\IniException $e) {
+    }
+}
+if (!empty($_GET['deactivate_ws'])) {
+    try {
+        deactivate_item(addslashes($_GET['deactivate_ws']));
+    } catch (\Jelix\IniFile\IniException $e) {
+    }
+}
+
+if (!empty($_GET['activate_ws'])) {
+    try {
+        activate_item(addslashes($_GET['activate_ws']));
+    } catch (\Jelix\IniFile\IniException $e) {
+    }
+}
+
+if (!empty($_GET['deactivate_mod'])) {
+    try {
+        deactivate_module(addslashes($_GET['deactivate_mod']));
+    } catch (\Jelix\IniFile\IniException $e) {
+    }
+}
+
+if (!empty($_GET['activate_mod'])) {
+    try {
+        activate_module(addslashes($_GET['activate_mod']));
+    } catch (\Jelix\IniFile\IniException $e) {
+    }
+}
+
+if (!empty($_GET['action'])) {
+    runServerCommand(addslashes($_GET['action']));
+}
+
+if (!empty($_GET['sync'])) {
+    try {
+        sync_from_steam();
+    } catch (Exception $e) {
+
+    }
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>m1 Zombieworld</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- Custom styles for this template -->
+    <link href="css/cover.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+
+<div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+        <div class="inner">
+            <h3 class="masthead-brand">Project Zomboid</h3>
+        </div>
+    </header>
+
+    <main role="main" class="inner cover" style="width: 1024px; margin-left: -12rem">
+        <h1 class="cover-heading">Mods</h1>
+        <?php $mods = get_mods_from();?>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col d-flex flex-column vh-100" style="max-height: 20em;">
+                    <div class="row flex-grow-1 overflow-auto border">
+                        <div class="col">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Workshop ID</th>
+                                    <th scope="col">Mod Name</th>
+                                    <th scope="col" style="width: 380px">Mod Modules</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+                                function printTableList($list)
+                                {
+                                        foreach ($list as $id=>$module) {
+                                            $module = (object) $module;
+                                            if ($module->active) {
+                                                echo "<span style='color: #198754; display: block'>".$module->name;
+                                                echo ' <a href="index.php?deactivate_mod=' . $id . '"><span class="fa-solid fa-lock" style="color: #3551dc"></span></a> ';
+                                            }
+                                            else {
+                                                echo "<span style='color: #dc3545; display: block'>".$module->name;
+                                                echo ' <a href="index.php?activate_mod=' . $id . '"><span class="fa-solid fa-lock-open" style="color: #6ecb35"></span></a> ';
+                                            }
+                                            echo "</span>";
+                                        }
+                                }
+
+                                foreach ($mods as $id=> $mod) {
+                                    $mod = (object) $mod;
+                                    if ($mod->active == 1) {
+                                        $name = "<span style='color: #198754; display: block'>".$mod->name."</span>";
+                                    }
+                                    else {
+                                        $name = "<span style='color: #dc3545; display: block'>".$mod->name."</span>";
+                                    }
+                                    echo "<tr><td>".$id."</td><td>".$name."</td><td>";
+                                    printTableList($mod->list);
+                                    echo '</td><td>';
+                                    if ($mod->active == 1) {
+                                        echo '<a href="index.php?deactivate_ws=' . $id . '"><span class="fa-solid fa-lock" style="color: #3551dc"></span></a> ';
+                                    }
+                                    else {
+                                        echo '<a href="index.php?activate_ws=' . $id . '"><span class="fa-solid fa-lock-open" style="color: #6ecb35"></span></a> ';
+                                    }
+                                    echo '<a href="index.php?delete='.$id.'"><span class="fa-solid fa-trash" style="color: #dc3545"></span></a> ';
+                                    echo '</td>';
+                                    echo "</tr>";
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+    <main role="main" class="inner cover">
+        <h1 class="cover-heading">Add Mod</h1>
+        <form method="post" action="index.php">
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" style="width: 120px;">Workshop ID</span>
+                </div>
+                <input type="number" class="form-control" name="wsid">
+            </div>
+            <input type="submit" class="btn btn-primary" value="Add Mod"></input>
+        </form>
+        <button type="button" class="btn btn-success" onclick="syncfromSteam()">Sync From Steam</button>
+    </main>
+    <main role="main" class="inner cover">
+        <h1 class="cover-heading">Control Server</h1>
+        <button type="button" class="btn btn-success" onclick="action('start')">Start Server</button>
+        <button type="button" class="btn btn-danger" onclick="action('stop')">Stop Server</button>
+        <button type="button" class="btn btn-primary" onclick="action('update')">Update Server</button>
+        <button type="button" class="btn btn-warning" onclick="action('restart')">Restart Server</button>
+    </main>
+    <footer class="mastfoot mt-auto">
+        <div class="inner">
+            <p></p>
+        </div>
+    </footer>
+</div>
+
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script>
+    window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.9.2/umd/popper.min.js" integrity="sha512-2rNj2KJ+D8s1ceNasTIex6z4HWyOnEYLVC3FigGOmyQCZc2eBXKgOxQmo3oKLHyfcj53uz4QMsRCWNbLd32Q1g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+<script>
+    function action(name) {
+        window.location.href = "index.php?action=" + name;
+    }
+    function syncfromSteam() {
+        window.location.href = "index.php?sync=true";
+    }
+</script>
+</body>
+
+</html>
+
